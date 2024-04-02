42ad31ff742708f665d8a3860652e7bfbdf62ba5
vtinter <156923123+NguyenvantuanT@users.noreply.github.com>
2024-04-02 16:29:08 +0700

    #1

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:meditation_app/pages/wecome_page.dart';
+
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(), // Wrap your app
+  ),
+);
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: const WecomePage(),
+    );
+  }
+}
---
+++ lib/pages/wecome_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:meditation_app/components/app_text_styte.dart';
+import 'package:meditation_app/themes/themes.dart';
+
+class WecomePage extends StatefulWidget {
+  const WecomePage({super.key});
+
+  @override
+  State<WecomePage> createState() => _WecomePageState();
+}
+
+class _WecomePageState extends State<WecomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF8E97FD),
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20)
+                .copyWith(top: 40, bottom: 20),
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                const Expanded(
+                  flex: 1,
+                  child: Text(
+                    "Silent❤️Moon",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: kColorLightYellow,
+                    ),
+                  ),
+                ),
+                const Expanded(
+                  flex: 3,
+                  child: Column(
+                    children: [
+                      AppTextStyte(
+                        text: "Hi Afsar, Welcome",
+                        fontWeight: FontWeight.bold,
+                        color: kColorLightYellow,
+                        fontSize: 30,
+                      ),
+                      AppTextStyte(
+                        text: "to Silent Moon",
+                        color: kColorLightYellow,
+                        fontSize: 25,
+                      ),
+                      SizedBox(height: 8),
+                      AppTextStyte(
+                        text: "Explore the app, Find some peace of mind to",
+                        color: kColorLightGrey,
+                        fontSize: 15,
+                      ),
+                      AppTextStyte(
+                        text: "prepare for meditation.",
+                        color: kColorLightGrey,
+                        fontSize: 15,
+                      ),
+                      Text(""),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 4,
+                  child: Align(
+                    child: Image.asset("assets/wecome_images/bg2.png"),
+                  ),
+                ),
+                Flexible(child: ElevatedButton(child:Text("GET START") , onPressed: () {} ,))
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
